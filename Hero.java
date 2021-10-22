227660f59b7c0e01f03cba1a87a5a57811975253
syfr456 <sekarsyafira225@gmail.com>
2021-10-22 16:02:50 +0700

    first commit

---
+++ Hero.java
@@ -0,0 +1,71 @@
+public class Hero extends Human {
+
+    private int energy = 100;
+
+    public Hero(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void player() {
+        System.out.println("========= Player ========");
+        System.out.println("Name: " + this.name);
+        System.out.println("Age: " + this.age);
+        System.out.println("Energy: " + this.energy);
+        System.out.println("=========================");
+    }
+
+    void checkEnergy() {
+        if (energy <= 0) {
+            System.out.println("Energi Anda habis! Player " + this.name + " tamat!");
+            System.exit(0);
+        }
+    }
+
+    void run() {
+        checkEnergy();
+        System.out.println(this.name + " sedang berlari....");
+        energy -= 10;
+    }
+
+    void eat() {
+        checkEnergy();
+        System.out.println(this.name + " sedang makan....");
+        energy += 20;
+    }
+
+    void drink() {
+        checkEnergy();
+        System.out.println(this.name + " sedang minum....");
+        energy += 20;
+    }
+
+    void sit() {
+        checkEnergy();
+        System.out.println(this.name + " sedang duduk....");
+        energy += 10;
+    }
+
+    void jump() {
+        checkEnergy();
+        System.out.println(this.name + " sedang melompat....");
+        energy -= 20;
+    }
+
+    void attack() {
+        checkEnergy();
+        System.out.println(this.name + " sedang menyerang....");
+        energy -= 30;
+    }
+    /*
+     * Buat method untuk lari, makan, minum, lompat, dan duduk. Masing2 memiliki
+     * kriteria sebagai berikut: - pasang method checkHealth() di dalam method yang
+     * akan dibuat - Tampilkan pesan kegiatan yang sedang dilakukan, misal, jika
+     * lari: " sedang berlari..." - ubah energy sesuai kegiatan yang dilakukan: eat
+     * = +20 drink = +20 jump = -20 sit = +10 attack = -30
+     * 
+     * Contoh method seperti method run() diatas.
+     * 
+     */
+
+}
---
+++ Main.java
@@ -0,0 +1,51 @@
+class Main {
+  public static void main(String[] args) {
+    Hero hero = new Hero("Marsani", 35);
+
+    hero.player();
+
+    hero.walk();
+    hero.walk();
+    hero.walk();
+    hero.walk();
+    hero.walk();
+    hero.walk();
+    hero.walk();
+    hero.run();
+    hero.run();
+    hero.eat();
+    hero.eat();
+    hero.eat();
+    hero.drink();
+    hero.drink();
+    hero.jump();
+    hero.jump();
+    hero.jump();
+    hero.jump();
+    hero.jump();
+    hero.jump();
+    hero.sit();
+    hero.sit();
+    hero.sit();
+    hero.sit();
+    hero.sit();
+    hero.walk();
+    hero.walk();
+    hero.walk();
+    hero.walk();
+    hero.walk();
+    hero.walk();
+    hero.walk();
+    hero.walk();
+    hero.walk();
+    hero.walk();
+    hero.attack();
+    hero.attack();
+
+    // Group 9: walk 7x, run 2x, eat 3x, drink 2x, jump 6x, sit 5x, walk 10x, attack
+    // 2x
+
+    hero.player();
+
+  }
+}
